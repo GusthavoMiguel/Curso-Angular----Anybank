0eb32d69ff4e522221bea65531d33de748173f65
F3881347 Gusthavo Ramos Miguel <gusthavo.miguel@bb.com.br>
2025-08-15 12:31:50 -0300

    Projeto completo, utilizando inputs e outputs,signals, @for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { FormNovaTransacaoComponent } from './componentes/form-nova-transacao/form-nova-transacao.component';
+import { TipoTransacao, Transacao } from './modelos/transacao';
+import { ExtratoComponent } from './componentes/extrato/extrato.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [BannerComponent, FormNovaTransacaoComponent, ExtratoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'anybank';
+  transacoes = signal<Transacao[]>([]);
+
+  saldo = computed(() => {
+    return this.transacoes().reduce((acc, transacaoAtual) => {
+      switch (transacaoAtual.tipo) {
+        case TipoTransacao.DEPOSITO:
+          return acc + transacaoAtual.valor;
+        case TipoTransacao.SAQUE:
+          return acc - transacaoAtual.valor;
+        default:
+          throw new Error('Tipo de transação não identificado.');
+      }
+    }, 0);
+  });
+
+  processarTransacao(transacao: Transacao) {
+    if (
+      transacao.tipo === TipoTransacao.SAQUE &&
+      transacao.valor > this.saldo()
+    ) {
+      return alert('Saldo insuficiente!');
+    }
+    this.transacoes.update((listaAtual) => [transacao, ...listaAtual]);
+  }
 }
